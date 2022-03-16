6c4a6a13e89fa05896fa3b0274292d6b5a8ef0d7
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-16 20:25:39 -0300

    ok

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -7,7 +7,7 @@ class Tabelas {
         const sql = 'CREATE TABLE Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar (20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
 
         this.conexao.query(sql, erro => {
-            if(errro) {
+            if(erro) {
                 console.log(erro)
             } else {
                 console.log('Tabela Atendimentos criada com sucesso')
