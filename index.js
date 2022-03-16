cea3e2891018f3148e4957bc6b0c50bf5b6d01fd
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-16 10:38:31 -0300

    ok

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const consign = require('consign')
 
 const app = express()
@@ -7,7 +6,6 @@ const app = express()
 consign()
     .include('controllers')
     .into(app)
-
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
 
 
