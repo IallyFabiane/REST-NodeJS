d8ec311749cd88c60421cfc6ef72d5dbff81ba7b
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-16 10:28:48 -0300

    ok

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 
+const consign = require('consign')
+
 const app = express()
+
+consign()
+    .include('controllers')
+    .into(app)
 
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
 
-app.get('/atendimentos', (req, res) => res.send ('Você está na rota de atendimentos e está realizando um GET'))
+
