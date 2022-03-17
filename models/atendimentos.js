6c7de3c32932e21af7f835be94a518d2aaa72e33
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-17 09:13:59 -0300

    OK

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,8 +2,8 @@ const moment = require('moment')
 const conexao =  require('../infraestrutura/conexao')
 class Atendimento {
     adiciona(atendimento) {
-        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
         const atendimentoDatado = {...atendimento, dataCriacao, data}
         const sql = 'INSERT INTO Atendimentos SET ?'
         conexao.query(sql, atendimentoDatado, (erro, resultados) => {
