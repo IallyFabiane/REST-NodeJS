32096f80a1ac28c37113d8f9ae48c782da1ba50a
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-17 08:43:32 -0300

    ok

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,8 +1,10 @@
 const conexao =  require('../infraestrutura/conexao')
 class Atendimento {
     adiciona(atendimento) {
+        const dataCriacao = new Date()
+        const atendimentoDatado = {...atendimento, dataCriacao}
         const sql = 'INSERT INTO Atendimentos SET ?'
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if(erro) {
                 console.log(erro)
             } else {
