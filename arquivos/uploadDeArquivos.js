ec62057d8f5f3278573f50734306367c4921b498
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-22 20:46:34 -0300

    ok

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,5 +1,7 @@
 const fs = require ('fs');
-
+module.exports = () => {
+    
+}
 fs.createReadStream('./assets/salsicha.jpg')
     .pipe(fs.createWriteStream('./assets/salsicha_stream.jpg'))
     .on('finish', () => console.log('Imagem foi escrita com sucesso'))
